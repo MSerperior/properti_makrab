39c1b5feb2f74708b623d220b9d6ff17adc61da1
Dany Christian <45038103+MSerperior@users.noreply.github.com>
2023-11-18 17:53:34 +0700

    update(keypad): add logic for activation and timer

--- testKeypad/testKeypad.ino
+++ testKeypad/testKeypad.ino
@@ -11,6 +11,9 @@ char keys[ROWS][COLS] = {
 };
 byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {11, 10, 9, 8}; //connect to the column pinouts of the keypad
+char keyPressed = '0';
+const String passcode = "123456";
+String inputcode = "";
 
 //initialize an instance of class NewKeypad
 Adafruit_Keypad customKeypad = Adafruit_Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);
@@ -18,19 +21,39 @@ Adafruit_Keypad customKeypad = Adafruit_Keypad( makeKeymap(keys), rowPins, colPi
 void setup() {
   Serial.begin(9600);
   customKeypad.begin();
+  pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  customKeypad.tick();
+  if(passcode == inputcode) startTimer();
+  if(inputcode.length() >= 6) inputcode="";
 
+  customKeypad.tick();
+  
   while(customKeypad.available()){
     keypadEvent e = customKeypad.read();
+    keyPressed = (char)e.bit.KEY;
     Serial.print((char)e.bit.KEY);
-    if(e.bit.EVENT == KEY_JUST_PRESSED) Serial.println(" pressed");
+    if(e.bit.EVENT == KEY_JUST_PRESSED) {
+      Serial.println(" pressed"); 
+      inputcode += keyPressed;
+    }
     else if(e.bit.EVENT == KEY_JUST_RELEASED) Serial.println(" released");
+    Serial.println(inputcode);
   }
 
   delay(10);
 }
+
+void startTimer() {
+  for(int i=60; i>0; i--){
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+  }
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
